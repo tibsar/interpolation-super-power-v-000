d4e12ae104af23590f55b255b62cf87c1bbbf9bc
Sara Tibbetts <sxt4866@rit.edu>
2018-02-19 21:45:49 +0000

    automatically backed up by learn

--- spec/display_rainbow_spec.rb
+++ spec/display_rainbow_spec.rb
@@ -1,7 +1,7 @@
 describe '#display_rainbow' do
   it 'accepts one argument' do
     colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
-
+    
     allow(self).to receive(:puts)
 
     expect { display_rainbow(colors) }.to_not raise_error(NoMethodError)
